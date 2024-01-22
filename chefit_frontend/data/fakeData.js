7589a861b3150d0fbffa810c2471656d85b08fbf
jaden416 <jaden416@outlook.com>
2024-01-22 14:57:36 -0500

    new stuff

---
+++ chefit_frontend/data/fakeData.js
@@ -0,0 +1,21 @@
+export const slider1 = [
+  {
+    image: require("../assets/food/1.jpg"),
+    title: "Pumpkin Cheesecake With Bourbon-Sour Cream Topping",
+    description: "The carmelized flavor of bourbon and the tang of sour cream add balance to the rich, spiced filling of this chees...",
+    rating: 4
+  },
+  {
+    image: require("../assets/food/2.jpg"),
+    title: "Pumpkin Cheesecake With Bourbon-Sour Cream Topping",
+    description: "The carmelized flavor of bourbon and the tang of sour cream add balance to the rich, spiced filling of this chees...",
+    rating: 5
+
+  },
+  {
+    image: require("../assets/food/3.jpg"),
+    title: "Pumpkin Cheesecake With Bourbon-Sour Cream Topping",
+    description: "The carmelized flavor of bourbon and the tang of sour cream add balance to the rich, spiced filling of this chees...",
+    rating: 3
+  },
+];
--- chefit_frontend/screens/Explore.js
+++ chefit_frontend/screens/Explore.js
@@ -1,23 +1,22 @@
 import React, {useEffect} from 'react'
+import Svg, { Path } from "react-native-svg"
+
 import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 import { StyleSheet, TextInput, View, ScrollView, Text, Dimensions, Image } from 'react-native';
 import {ScaledSheet} from 'react-native-size-matters';
 import { SafeAreaView} from 'react-native-safe-area-context';
+import { slider1 } from '../data/fakeData.js'
 
-
-export const assets = [
-  require("../assets/food/burger.png"),
-  require("../assets/food/burger2.png"),
-];
 
 const { width, height } = Dimensions.get("window");
 
 
 export default function Explore() {
   useEffect(()=>{
-    console.log(assets)
+    // console.log(assets)
   },[])
-    return <SafeAreaView >
+    return <SafeAreaView  className="bg-[#FDFEFC]">
+      <ScrollView>
       <View style ={styled.base}>
         <View className=' flex-row items-center justify-center'>
           <View className='flex-row space-x-2  mx-2 bg-[#FEF3CD] border-2 border-[#FEF3CD] rounded-lg p-2'>
@@ -30,7 +29,14 @@ export default function Explore() {
         </View>
       </View>
 
-      <Swiper/>
+
+
+      <Swiper
+        items={slider1}
+      />
+      <Swiper
+        items={slider1}
+      />
 
       {/* <View className='w-full h-fit bg-gray-100 border-2 border-gray-300 rounded-md   mb-8'>
       <Text className={`text-lg underline-offset-[3px] font-bold border-gray-300 border-b-2 p-2`}>Current Schedule</Text>
@@ -40,26 +46,47 @@ export default function Explore() {
       <View className= 'w-full'>
       </View>
     </View> */}
+    </ScrollView>
   </SafeAreaView>
 }
 
-const Swiper = () => {
+const Swiper = ({items}) => {
 
-  return <View>
+  return <View >
     <ScrollView 
       showsHorizontalScrollIndicator={false}
       snapToInterval={width} 
       decelerationRate={"fast"} horizontal
     >
-      {assets.map((source)=>(
-        <View key={source} style={styles.picture} className='p-2'>
-          <View className='w-full  bg-gray-100 border-2 border-gray-300 rounded-md  mb-8 '>
-              <Image className='w-full   bg-gray-100 border-2 border-gray-300 rounded-md  ' {...{source}}/>
-            <Text className={`text-lg underline-offset-[3px] font-bold border-gray-300 border-b-2 p-2`}>Burger</Text>
-            <Text className={`text-lg  font-bold border-gray-300 border-b-2 p-2`}>The carmelized flavor of bourbon and the tang of sour cream add balance to the rich, spiced filling of this cheese</Text>
+
+      {items.map((source)=>(
+        <View key={source} style={styles.picture} className='p-2 '>
+          <View className='w-full  mb-8'>
+            <View>
+              <Image className='w-full ' source={source.image}/>
+            </View>
+            <View className="border-l-2 border-r-2 border-b-2 rounded-bl-md rounded-br-md border-gray-300">
+            <View className={`p-2`}>
+              <Text className={`text-lg font-bold`}>{source.title}</Text>
+            </View>
+            <View className={`p-2`}>
+              <Text >{source.description}</Text>
+            </View>
+            <View>
+              <View className={"p-2 flex flex-row gap-1"}>
+              {Array.from({ length: source.rating }, () => (
+                <Svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <Path d="M8.48075 0.556577L6.16169 4.69357L0.973099 5.35911C0.0426336 5.47784 -0.330263 6.48709 0.344502 7.06515L4.09833 10.2835L3.21048 14.8298C3.05067 15.6516 4.0344 16.2671 4.85833 15.8828L9.5 13.7362L14.1417 15.8828C14.9656 16.264 15.9493 15.6516 15.7895 14.8298L14.9017 10.2835L18.6555 7.06515C19.3303 6.48709 18.9574 5.47784 18.0269 5.35911L12.8383 4.69357L10.5193 0.556577C10.1037 -0.180831 8.89981 -0.190205 8.48075 0.556577Z" fill="#F7D47A"/>
+                </Svg>
+              ))}
+              </View>
+            </View>
+            </View>
+           
           </View> 
       </View>
       ))}
+      
     </ScrollView>
   </View>
 }
@@ -77,7 +104,7 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
   },
   pictures: {
-    width: width * assets.length,
+    // width: width * slider1.length,
     height,
     flexDirection: "row",
   },
