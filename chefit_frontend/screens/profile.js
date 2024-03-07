8478cd60c007a40439de186fb1c5b02ee8570c08
mmya2300 <mmya230300@gmail.com>
2024-03-07 12:34:11 -0500

    user auth ready to go

--- chefit_frontend/screens/profile.js
+++ chefit_frontend/screens/profile.js
@@ -1,284 +1,100 @@
-import React, { useState } from 'react';
-import { View, Text, Image, Pressable, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Tab, TabView } from '@rneui/themed';
-import BackButton from '../components/BackButton';
-import AchievementsButton from "../components/selectAchievement";
-import DisplayCooksonas from '../subScreens/DisplayCooksonas';
-import { testuserInfo } from '../API/data';
-import DisplayAchievements from '../subScreens/DisplayAchievements';
-import SettingsScreen from '../subScreens/Settings';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { auth } from '../config/firebase'; // Assuming you have initialized Firebase Auth
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import { db } from '../config/firebase';
 
+export default function ProfileScreen({ navigation }) {
+    const [userEmail, setUserEmail] = useState('');
+    const [userUID, setUserUID] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [unsubscribe, setUnsubscribe] = useState(null);
 
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const user = auth.currentUser;
+                if (user) {
+                    setUserEmail(user.email);
+                    const currentUserUID = user.uid;
+                    setLoading(true);
+                    const usersQuery = query(collection(db, 'users'), where('UID', '==', currentUserUID));
+                    const newUnsubscribe = onSnapshot(usersQuery, (snapshot) => {
+                        let userList = [];
+                        snapshot.docs.map((doc) => userList.push({ ...doc.data(), id: doc.id }));
+                        if (userList.length > 0) {
+                            setUserUID(userList[0].UID);
+                        }
+                        setLoading(false);
+                    });
+                    setUnsubscribe(() => newUnsubscribe);
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
 
-const Stack = createStackNavigator();
+        fetchUserData();
 
-const TabComponent = ({navigation}) => {
-  const [index, setIndex] = useState(0);
+        return () => {
+            if (unsubscribe) {
+                unsubscribe();
+            }
+        };
+    }, []);
 
-  const MyRecipes = () => (
-    <View style={styles.recipeCard}>
-      
-      <Image 
-          source={require('../assets/chickenAndRiceSoup_recipecardimage.png')}
-          style={{width: 184, height: 140, resizeMode: 'cover', borderTopRightRadius: 20, borderTopLeftRadius: 20, marginBottom: 10}}
-        />
-      <View style={styles.recipeCardHeading}>
-        <Text>
-          Chicken and Rice Soup
-        </Text>
-      </View>
-      <View>
-        <Text style={{color: 'grey', fontWeight: '100', fontSize: 10}}>
-          By: Username01
-        </Text>
-      </View>
-      <View style={styles.timeContainer}>
-        <Text style={{ color: '#47A695', fontWeight: '100', fontSize: 12}}>
-          40 mins
-        </Text>
-        <Image 
-          source={require('../assets/navIcons/time.png')}
-          style={{width: 13, height: 13, resizeMode: 'contain', margin: 3,}}
-        />
-      </View>
-      <View style={{flex: 1, flexDirection: 'row', gap: 85, padding: 3}}>
-        <Image 
-          source={require('../assets/sampleStars.png')}
-          style={{width: 72, height: 15, resizeMode: 'contain',}}
-        />
-        <Image 
-          source={require('../assets/actionIcons/savedRecipe_inactive.png')}
-          style={{width: 10, height: 16, resizeMode: 'contain',}}
-        />
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+                    <Image source={require('../assets/actionIcons/backButton.png')} style={styles.backButtonImage} />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.content}>
+                <Text style={styles.titleText}>Profile</Text>
+                <Text style={styles.emailText}>Email: {userEmail}</Text>
+                <Text style={styles.uidText}>User UID: {userUID}</Text>
+            </View>
         </View>
-        
-      
-    </View>
-  );
-
-  const MyAchievements = () => (
-        <View style={styles.Achievementcontainer}>
-              {testuserInfo.userAchievements.map((achievement, index) => (
-                <AchievementsButton
-                  key={index}
-                  title={achievement.trigger ? achievement.title : "Locked Achievement"}
-                  description={achievement.trigger ? achievement.UnlockedDescription : achievement.LockedDescription}
-                  unlockedImage={achievement.trigger ? achievement.UnlockedImage : achievement.LockedImage}
-                  navigation={navigation}
-                  timeTriggered={achievement.timeTriggered}
-                  rewardImage={achievement.trigger ? achievement.rewardImage : achievement.lockedReward}
-                />
-              ))}
-        </View>
-  );
-
-  return (
-    <View>
-      <Tab
-        value={index}
-        onChange={(e) => setIndex(e)}
-        indicatorStyle={{
-          backgroundColor: 'white',
-          height: 3,
-          elevation: 5,
-          shadowColor: 'rgba(0, 0, 0, 0.25)',
-          shadowOffset: { width: 0, height: 4 },
-          shadowOpacity: 1,
-          shadowRadius: 4,
-        }}
-        buttonStyle={{ borderColor: 'transparent'}}
-        variant='default' disableIndicator={false}>
-        <Tab.Item title="My Recipes" titleStyle={(active) => ({ height: 50, width: 170, fontSize: 18, color: active ? "#42A797" : "grey", fontWeight: active ? "bold" : undefined, textDecorationLine: active ? 'underline' : 'none', textDecorationColor: active ? "#42A797" : "grey",})}
-          buttonStyle={(active) => ({ borderColor: active ? "#42A797" : "grey", borderBottomWidth: active ? 3 : 0.5 })} />
-        <Tab.Item title="Achievements" titleStyle={(active) => ({ height: 50, width: 170, fontSize: 18, color: active ? "#42A797" : "grey", fontWeight: active ? "bold" : undefined, textDecorationLine: active ? 'underline' : 'none', textDecorationColor: active ? "#42A797" : "grey", })}
-          buttonStyle={(active) => ({ borderColor: active ? "#42A797" : "grey", borderBottomWidth: active ? 3 : 0.5 })} />
-      </Tab>
-      <TabView value={index} onChange={setIndex} animationType="timing" disableSwipe={true} tabItemContainerStyle={{flex: 1,}}>
-        <TabView.Item>
-          <MyRecipes />
-        </TabView.Item>
-        <TabView.Item>
-          <MyAchievements />
-        </TabView.Item>
-      </TabView>
-    </View>
-  );
-}
-
-function Profile({ navigation }) {
-  return (
-      
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.header}>
-          <View style={styles.editButton}>
-            <Pressable onPress={() => navigation.navigate('Select your Cooksona')}>
-              <Image
-                source={require('../assets/actionIcons/editCooksonaIcon.png')}
-                style={styles.editIcon}
-              />
-            </Pressable>
-          </View>
-          <View style={styles.profileImageContainer}>
-            <Image
-              source={require('../assets/Cooksonas/pancakes.png')}
-              style={styles.profileImage}
-            />
-          </View>
-        </View>
-        <Text style={styles.username}>Username01</Text>
-        <View style={styles.tabContainer}>
-          <TabComponent navigation={navigation} />
-        </View>
-      </View>
-    </SafeAreaView>
-    
-
-
-
-  )
-}
-
-
-export default function ProfileScreen() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerBackTitleVisible: false,
-        headerBackImage: () => (
-          <BackButton />
-        ),
-        headerTitleAlign: 'center',
-        headerTintColor: '#000',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-        headerStyle: {
-          elevation: 0, 
-          shadowOpacity: 0, 
-          borderBottomWidth: 0,
-        }
-      }}
-    >
-      <Stack.Screen
-  name='My Profile'
-  component={Profile}
-  options={({ navigation }) => ({
-    headerTitleAlign: 'left',
-    
-    headerRight: () => (
-      <View>
-        <Pressable
-          onPress={() => navigation.navigate('Settings Screen')}>
-          <Image
-            source={require('../assets/actionIcons/settingsButton.png')}
-            style={{ width: 32, height: 32, resizeMode: 'cover', margin: 30 }}
-          />
-        </Pressable>
-      </View>
-    ),
-  })}
-
-/>
-
-      <Stack.Screen
-        name='Settings Screen'
-        component={SettingsScreen}
-        options={{
-          headerTitle: 'Settings',
-          headerStyle: {
-            elevation: 0, 
-            shadowOpacity: 0, 
-            borderBottomWidth: 0,
-          },
-          headerShown: false
-          
-        }}
-      />
-       <Stack.Screen
-        name='Select your Cooksona'
-        component={DisplayCooksonas}
-        options={{
-          headerTitleAlign: 'left',
-          headerTitle: '',
-        }}
-      />
-       <Stack.Screen
-        name='Display Achievements'
-        component={DisplayAchievements}
-        options={{
-          headerTitle: '',
-        }}
-      />
-    </Stack.Navigator>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    
-  },
-  Achievementcontainer: {
-    margin: 10
-  },
-  content: {
-    flex: 1,
-    alignItems: "center",
-  },
-  header: {
-    alignItems: "center",
-    width: 200,
-    height: 200,
-    backgroundColor: '#F9B59E',
-    borderRadius: 10,
-    marginBottom: 20,
-  },
-  editButton: {
-    position: 'absolute',
-    bottom: 5,
-    right: 8,
-  },
-  editIcon: {
-    width: 22,
-    height: 22,
-  },
-  profileImageContainer: {
-    position: 'absolute',
-    top: 20,
-  },
-  profileImage: {
-    width: 160,
-    height: 154,
-  },
-  username: {
-    textAlign: "center",
-    marginBottom: 20,
-    fontWeight: 'bold',
-    fontSize: 24,
-  },
-  tabContainer: { 
-    marginTop: 16,
-    width: "100%",
-  },
-  recipeCard:{
-    margin: 30,
-    width: 185,
-    height: 245,
-    borderRadius: 20,
-    backgroundColor: '#FFF',
-    borderWidth: 1,
-    borderColor: '#E1E1E1',
-    elevation: 5,
-    
-},
-  timeContainer:{
-    flex: 1,
-    flexDirection: 'row',
-    width: 61,
-    height: 16,
-    
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#F8FAF8',
+    },
+    header: {
+        padding: 20,
+    },
+    backButton: {
+        width: 35,
+        height: 35,
+    },
+    backButtonImage: {
+        width: '100%',
+        height: '100%',
+    },
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    titleText: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#42A797',
+    },
+    emailText: {
+        fontSize: 20,
+        color: '#333',
+        marginBottom: 10,
+    },
+    uidText: {
+        fontSize: 20,
+        color: '#333',
+        marginBottom: 20,
+    },
+    // Add more styles as needed
 });
--- chefit_frontend/screens/signUp.js
+++ chefit_frontend/screens/signUp.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import { useState } from 'react'
-import { StyleSheet, View, Button, TextInput, TouchableOpacity, Image } from 'react-native';
-import { Text } from '@rneui/themed'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../config/firebase'; // Import Firebase Auth and Firestore
+import { collection, setDoc, doc } from 'firebase/firestore'; // Import Firestore methods
 
-export default function SignUpScreen({navigation}) {
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmpassword, setConfirmpassword] = useState('')
-    const [email, setEmail] = useState('')
 
-    // Validate First Name 
-    const [checkFName, setCheckFName] = useState(false)
+export default function SignUpScreen({ navigation }) {
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmpassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+
+    // Validation states
+    const [checkFName, setCheckFName] = useState(false);
+    const [checkLName, setCheckLName] = useState(false);
+    const [checkUpper, setCheckUpper] = useState(false);
+    const [checkLower, setCheckLower] = useState(false);
+    const [checkNum, setCheckNum] = useState(false);
+    const [checkNonAlphaNum, setCheckNonAlphaNum] = useState(false);
+    const [checkConfirmPass, setCheckConfirmPass] = useState(false);
+    const [checkEmail, setCheckEmail] = useState(false);
+    const [reg, checkReg] = useState(true)
 
     const handleFName = (e) => {
-        let regex = /^[^\d=?\\/@#%^&*()]+$/
-        setFirstname(e)
-        if (regex.test(e)) {
-            setCheckFName(false)
-        } else {
-            setCheckFName(true)
-        }
-    }
+      let regex = /^[^\d=?\\/@#%^&*()]+$/;
+      setFirstname(e);
+      if (regex.test(e)) {
+          setCheckFName(false);
+      } else {
+          setCheckFName(true);
+      }
+  };
 
-    // Validate Last Name 
-    const [checkLName, setCheckLName] = useState(false)
+  const handleLName = (e) => {
+      let regex = /^[^\d=?\\/@#%^&*()]+$/;
+      setLastname(e);
+      if (regex.test(e)) {
+          setCheckLName(false);
+      } else {
+          setCheckLName(true);
+      }
+  };
 
-    const handleLName = (e) => {
-        let regex = /^[^\d=?\\/@#%^&*()]+$/
-        setLastname(e)
-        if (regex.test(e)) {
-            setCheckLName(false)
-        } else {
-            setCheckLName(true)
-        }
-    }
-
-    // Validate Password 
-    const [checkUpper, setCheckUpper] = useState(false)
-    const [checkLower, setCheckLower] = useState(false)
-    const [checkNum, setCheckNum] = useState(false)
-    const [checkNonAlphaNum, setCheckNonAlphaNum] = useState(false)
-    const handlePassword = (e) => {
+  const handlePassword = (e) => {
         const containsUppercase = /^(?=.*[A-Z]).*$/
         const containsLowercase = /^(?=.*[a-z]).*$/
         const containsNumber = /^(?=.*[0-9]).*$/
@@ -82,8 +83,6 @@ export default function SignUpScreen({navigation}) {
         } 
     }
 
-    // Validate Confirm Password
-    const [checkConfirmPass, setCheckConfirmPass] = useState(false)
     const handleConfirmPass = (e) => {
         setConfirmpassword(e)
         if (password === e) {
@@ -94,31 +93,70 @@ export default function SignUpScreen({navigation}) {
         }
     }
 
-    // Validate Email
-    const [checkEmail, setCheckEmail] = useState(false)
-
     const handleEmail = (e) => {
-        let regex = /[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\.[a-zA-Z]/
-        setEmail(e)
-        if (regex.test(e)) {
-            setCheckEmail(false)
-        } else {
-            setCheckEmail(true)
+      let regex = /[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\.[a-zA-Z]/
+      setEmail(e)
+      if (regex.test(e)) {
+          setCheckEmail(false)
+      } else {
+          setCheckEmail(true)
+      }
+  }
+
+
+  const handleRegistration = async () => {
+    if (!reg && !checkFName && !checkLName && !checkUpper && !checkLower && !checkNum && !checkNonAlphaNum && !checkConfirmPass && !checkEmail) {
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            const usersCollectionRef = collection(db, 'users');
+            await setDoc(doc(usersCollectionRef, username), {
+                name: firstname + ' ' + lastname,
+                username: username,
+                UID: user.uid,
+                achievements: [{
+                  "achievementID": "achieve001",
+                  "title": "Recipe Pioneer",
+                  "UnlockedDescription": "Create your first Recipe!",
+                  "LockedDescription": "Hint: Try adding a Recipe!",
+                  "trigger": false,
+                  "UnlockedImage" : "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2Ffirstrecipe_logo.png?alt=media&token=24333059-8360-4271-961f-e80bb4ec6eae",
+                  "LockedImage": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2FlockedAchievement.png?alt=media&token=d5861753-3343-4d85-b658-cbd09d513e03",
+                  "lockedReward": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2FlockedAchievement_image.png?alt=media&token=450f75f1-a3c8-4134-82d8-d3abf963834c",
+                  "rewardImage": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2Ffirstrecipe_rewardImage.png?alt=media&token=7e064dce-9222-4c82-8cf1-22830b340235",
+                  "timeTriggered": "",
+                }, {
+                  
+                    "LockedDescription": "Hint: Go check out our Cooksonas!",
+                    "LockedImage": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2FlockedAchievement.png?alt=media&token=d5861753-3343-4d85-b658-cbd09d513e03",
+                    "UnlockedDescription": "Changed your Cooksona for the first time!",
+                    "UnlockedImage": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2Ffacelift_logo.png?alt=media&token=ea226e05-2e4e-48c9-87dd-a51dbd5374d7",
+                    "achievementID": "achieve002",
+                    "lockedReward": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2FlockedAchievement_image.png?alt=media&token=450f75f1-a3c8-4134-82d8-d3abf963834c",
+                    "rewardImage": "https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FachievementImages%2Ffacelift_rewardImage.png?alt=media&token=3fc6dc73-f683-4a3a-92a8-05f7b4fe519c",
+                    "title": "Flavorful Facelift",
+                    "trigger": false,
+                    "timeTriggered": "",
+                }],
+                recipes:[],
+                savedRecipes:[],
+                userAvatar:"https://firebasestorage.googleapis.com/v0/b/chef-it-fdbea.appspot.com/o/images%2FCooksonas%2Fpancakes_withbackground.png?alt=media&token=38d2e61e-f2f5-42e5-b061-0e310b3ea17c",
+                userIngredients:[],
+            });
+
+            navigation.navigate('Home');
+        } catch (error) {
+            setError(error.message);
         }
     }
-
-    const [reg, checkReg] = useState(true)
-    const checkRegistration = () => {
-        if (!reg && !checkFName && !checkLName && !checkUpper && !checkLower && !checkNum && !checkNonAlphaNum && !checkConfirmPass && !checkEmail) {
-            navigation.navigate("Home")
-        } 
-    }
+};
 
     return (
-    <View style={styles.container}>
+       <View style={styles.container}>
       <View style={styles.backContainer}>
         <TouchableOpacity style={styles.backButton} onPress={()=> navigation.goBack()}>
-          <Image source={require('../assets/buttons/backButton.png')} style={styles.backButton} />
+          <Image source={require('../assets/actionIcons/backButton.png')} style={styles.backButton} />
         </TouchableOpacity>
       </View>
       <View style={styles.contentContainer}>
@@ -158,97 +196,97 @@ export default function SignUpScreen({navigation}) {
       {checkConfirmPass? (
         <Text style={styles.errorText}>Does not match password entered</Text> ) : ( <Text></Text>)
       }
-      <TouchableOpacity style={styles.button} onPress={() => checkRegistration()}>
+      <TouchableOpacity style={styles.button} onPress={() => handleRegistration()}>
         <Text style={styles.buttonText}>SIGN UP</Text>
       </TouchableOpacity>
       </View>
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#F8FAF8',
-    minHeight: "100%"
-  },
-  contentContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  backContainer: {
-    flexDirection: 'row',
-    alignItem: "left",
-    justifyContent: "left",
-    backgroundColor: "#F8FAF8",
-    padding: 10,
-    marginTop: 30
-  },
-  backButton: {
-    width: 35, 
-    height: 35, 
-    paddingLeft: 10,
-    shadowColor: "#494949",
+    container: {
+        backgroundColor: '#F8FAF8',
+        minHeight: '100%',
+    },
+    contentContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    backContainer: {
+        flexDirection: 'row',
+        alignItems: 'left',
+        justifyContent: 'left',
+        backgroundColor: '#F8FAF8',
+        padding: 10,
+        marginTop: 30,
+    },
+    backButton: {
+        width: 35,
+        height: 35,
+        paddingLeft: 10,
+        shadowColor: '#494949',
         shadowOffset: {
             width: 0,
             height: 5,
         },
-    shadowOpacity: 0.5
-  },
+        shadowOpacity: 0.5,
+    },
     titleText: {
-      fontSize: 50,
-      color: "#F7B49B",
-      fontFamily: 'Coiny_400Regular',
-      padding: 15,
-      maeginTop: 30
+        fontSize: 50,
+        color: '#F7B49B',
+        fontFamily: 'Coiny_400Regular',
+        padding: 15,
+        marginTop: 30,
     },
     introText: {
-      fontSize: 18,
-      paddingTop: 10,
-      color: "#42A797",
-      marginBottom: 25
+        fontSize: 18,
+        paddingTop: 10,
+        color: '#42A797',
+        marginBottom: 25,
     },
     button: {
-        backgroundColor: "#42A797",
+        backgroundColor: '#42A797',
         padding: 12,
         paddingHorizontal: 50,
         borderRadius: 50,
         margin: 15,
         marginTop: 20,
-        shadowColor: "#494949",
+        shadowColor: '#494949',
         shadowOffset: {
             width: 0,
             height: 5,
         },
-        shadowOpacity: 0.3
+        shadowOpacity: 0.3,
     },
     buttonText: {
-      fontSize: 22,
-      fontFamily: 'Coiny_400Regular',
-      color: "white"
+        fontSize: 22,
+        fontFamily: 'Coiny_400Regular',
+        color: 'white',
     },
     loginContainer: {
         width: '100%',
-        paddingHorizontal: 30
+        paddingHorizontal: 30,
     },
     input: {
         backgroundColor: 'white',
         borderWidth: 1,
-        borderColor: "#E8E8E8",
+        borderColor: '#E8E8E8',
         paddingHorizontal: 15,
         paddingVertical: 15,
         marginVertical: 3,
         borderRadius: 30,
-        shadowColor: "#494949",
-            shadowOffset: {
-                width: 0,
-                height: 3,
-            },
-        shadowOpacity: 0.3
+        shadowColor: '#494949',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.3,
     },
     errorText: {
-      marginTop: 5,
-      color: "#fc0330",
-      fontWeight: 'bold',
-      textAlign: "center"
-    }
+        marginTop: 5,
+        color: '#fc0330',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
