e24a823c0bf389ca158d746fe14e8538f7f04b5d
jaden416 <jaden416@outlook.com>
2024-04-11 20:46:57 -0400

    reset

--- chefit_frontend/subScreens/ResetUsername.js
+++ chefit_frontend/subScreens/ResetUsername.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Image, StyleSheet, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { db, auth } from '../config/firebase'; // Import your Firebase Firestore configuration
+import { query, collection, where, getDocs, setDoc, deleteDoc, doc, updateDoc} from 'firebase/firestore'; // Import necessary Firestore functions
+
+
 
 export default function ResetUsername({ navigation }) {
     const [username, setUsername] = useState('');
@@ -8,15 +12,13 @@ export default function ResetUsername({ navigation }) {
     const [usernameValidation, setUsernameValidation] = useState({
         checkUpper: false,
         checkLower: false,
-        checkNum: false,
-        checkNonAlphaNum: false,
         checkMinLength: false,
     });
+    
+    const [successMessage, setSuccessMessage] = useState('');
 
     const containsUppercase = /^(?=.*[A-Z]).*$/;
     const containsLowercase = /^(?=.*[a-z]).*$/;
-    const containsNumber = /^(?=.*[0-9]).*$/;
-    const containsNonAlphaNum = /^(?=.*[^a-zA-Z0-9]).*$/;
 
     const handleUsernameChange = (text) => {
         setUsername(text);
@@ -24,8 +26,6 @@ export default function ResetUsername({ navigation }) {
             ...usernameValidation,
             checkUpper: containsUppercase.test(text),
             checkLower: containsLowercase.test(text),
-            checkNum: containsNumber.test(text),
-            checkNonAlphaNum: containsNonAlphaNum.test(text),
             checkMinLength: text.length >= 8,
         });
     };
@@ -34,13 +34,71 @@ export default function ResetUsername({ navigation }) {
         setConfirmUsername(text);
     };
 
-    const handleResetUsername = () => {
-        // this is for handling the backend reset later
+    const handleResetUsername = async () => {
+        
+            try {
+
+                if(username !== confirmUsername){
+                    console.log(successMessage)
+                    setSuccessMessage("Make sure both fields match")
+                    return;
+                }
+
+                
+                const user = auth.currentUser;
+                if (!user) {
+                    console.log('User not authenticated');
+                    return;
+                }
+    
+                const currentUserUID = user.uid;
+                const usersQuery = query(collection(db, 'users'), where('UID', '==', currentUserUID));
+                const querySnapshot = await getDocs(usersQuery);
+    
+                if (querySnapshot.empty) {
+                    console.log("User data not found");
+                    return;
+                }
+    
+                const userData = querySnapshot.docs[0].data();
+                const oldUsername = userData.username;
+                console.log(userData.username)
+    
+                const usersCollection = collection(db, 'users');
+                
+                await updateDoc(doc(db, 'users', oldUsername), {
+                    username: username
+                })
+
+                const UpdateduserDoc = doc(usersCollection, username);
+
+                // Create a new document with the updated username
+                await setDoc(UpdateduserDoc, userData);
+
+                await updateDoc(doc(db, 'users', username), {
+                    username: username
+                })
+
+                console.log(userData.username)
+                // Delete the old document with the previous username
+                await deleteDoc(doc(usersCollection, oldUsername));
+                setSuccessMessage('Username reset successful');
+
+                console.log('Username reset successful');
+                
+    
+            } catch (error) {
+                setSuccessMessage('Username reset failed');
+                console.error('Error resetting username:', error);
+                
+            }
     };
+    
 
     return (
         <SafeAreaView style={styles.container} >
             <Text style={styles.heading}>Reset Username</Text>
+            
             <View style={styles.inputContainer}>
                 <TextInput
                     style={styles.textInput}
@@ -65,52 +123,33 @@ export default function ResetUsername({ navigation }) {
                     <View style={styles.requirementItem}>
                         {usernameValidation.checkMinLength 
                         ? (
-                            <Image source={require('../assets/buttons/item_checked.png')} style={{ width:  25, height:  25 }} />
+                            <Image source={require('../assets/actionIcons/item_checked.png')} style={{ width:  25, height:  25 }} />
                         ) : (
-                            <Image source={require('../assets/buttons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
+                            <Image source={require('../assets/actionIcons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
                         )}
                         <Text style={styles.text}>Min. 8 characters</Text>
                     </View>
 
                     <View style={styles.requirementItem}>
                         {usernameValidation.checkUpper ? (
-                            <Image source={require('../assets/buttons/item_checked.png')} style={{ width:  25, height:  25 }} />
+                            <Image source={require('../assets/actionIcons/item_checked.png')} style={{ width:  25, height:  25 }} />
                         ) : (
-                            <Image source={require('../assets/buttons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
+                            <Image source={require('../assets/actionIcons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
                         )}
                         <Text style={styles.text} >Uppercase Letter </Text>
                     </View>
 
                     <View style={styles.requirementItem}>
-                        {usernameValidation.checkNonAlphaNum ? (
-                            <Image source={require('../assets/buttons/item_checked.png')} style={{ width:  25, height:  25 }} />
+                        {usernameValidation.checkLower ? (
+                            <Image source={require('../assets/actionIcons/item_checked.png')} style={{ width:  25, height:  25 }} />
                         ) : (
-                            <Image source={require('../assets/buttons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
+                            <Image source={require('../assets/actionIcons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
                         )}
                         <Text style={styles.text}>Lowercase Letter</Text>
                     </View>
-
-                    <View style={styles.requirementItem}>
-                        {usernameValidation.checkNonAlphaNum ? (
-                            <Image source={require('../assets/buttons/item_checked.png')} style={{ width:  25, height:  25 }} />
-                        ) : (
-                            <Image source={require('../assets/buttons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
-                        )}
-                        <Text style={styles.text}>Special Character</Text>
-                    </View>
-
-                    <View style={styles.requirementItem}>
-                        {usernameValidation.checkNum 
-                        ? (
-                            <Image source={require('../assets/buttons/item_checked.png')} style={{ width:  25, height:  25 }} />
-                        ) : (
-                            <Image source={require('../assets/buttons/unchecked_button.png')} style={{ width:  25, height:  25 }} />
-                        )}
-                        <Text style={styles.text}>A Number</Text>
-                    </View>
-
-                    
                 </View>
+
+                {successMessage ? <Text style={styles.successMessage}>{successMessage}</Text> : null}
             
             <Pressable onPress={handleResetUsername} style={styles.button}>
                 <Text style={styles.buttonText}>Save</Text>
